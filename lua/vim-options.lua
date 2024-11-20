fab852389ff067942693a088f63827e1fcdcba7f
Juri <labaj.juraj@gmail.com>
2024-11-20 19:02:32 +0100

    Filez

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.cmd("set relativenumber")
+
+vim.g.mapleader = " "
+
+-- keybinds
+vim.keymap.set('n', "<leader>0", ":0<CR>", {})
+-- vim.keymap.set('n', "L", ":Lazy<CR>", {})
