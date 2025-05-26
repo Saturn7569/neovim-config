0f8676388effe7d6cbbd523d0193310cf0806a84
Saturn7569 <143340840+Saturn7569@users.noreply.github.com>
2025-05-26 13:32:59 +0200

    Started work on custom theme

--- lua/global_config.lua
+++ lua/global_config.lua
@@ -4,6 +4,31 @@ M.tab_size = 4
 
 M.global_theme = "catppuccin"
 
+local colors = {
+    crust = "#11111b",
+    red = "#f38ba8",
+    green = "#a6e3a1",
+    blue = "#89b4fa",
+    text = "#cdd6f4",
+    sb = "#313244",
+}
+local custom_catppuccin = {}
+custom_catppuccin.normal = {
+    a = {bg = colors.red, fg = colors.crust, gui = "bold"},
+    b = {bg = colors.sb, fg = colors.text},
+    c = {bg = colors.crust, fg = colors.sb},
+}
+custom_catppuccin.visual = {
+    a = {bg = colors.green, fg = colors.crust, gui = "bold"},
+    b = {bg = colors.sb, fg = colors.text},
+    c = {bg = colors.crust, fg = colors.sb},
+}
+custom_catppuccin.insert = {
+    a = {bg = colors.blue, fg = colors.crust, gui = "bold"},
+    b = {bg = colors.sb, fg = colors.text},
+    c = {bg = colors.crust, fg = colors.sb},
+}
+
 M.themes = {
     ["catppuccin"] = {
         flavor = "mocha",
