845ea766d96249011075d581e8288f021c937ec7
Saturn7569 <143340840+Saturn7569@users.noreply.github.com>
2025-05-26 13:40:32 +0200

    Custom catppuccin lualine theme

--- lua/global_config.lua
+++ lua/global_config.lua
@@ -9,30 +9,48 @@ local colors = {
     red = "#f38ba8",
     green = "#a6e3a1",
     blue = "#89b4fa",
+    mauve = "#cba6f7",
+    sap = "#74c7ec",
     text = "#cdd6f4",
+    textgray = "#585b70",
     sb = "#313244",
 }
 local custom_catppuccin = {}
 custom_catppuccin.normal = {
     a = {bg = colors.red, fg = colors.crust, gui = "bold"},
     b = {bg = colors.sb, fg = colors.text},
-    c = {bg = colors.crust, fg = colors.sb},
+    c = {bg = colors.crust, fg = colors.textgray},
 }
 custom_catppuccin.visual = {
     a = {bg = colors.green, fg = colors.crust, gui = "bold"},
     b = {bg = colors.sb, fg = colors.text},
-    c = {bg = colors.crust, fg = colors.sb},
+    c = {bg = colors.crust, fg = colors.textgray},
 }
 custom_catppuccin.insert = {
     a = {bg = colors.blue, fg = colors.crust, gui = "bold"},
     b = {bg = colors.sb, fg = colors.text},
-    c = {bg = colors.crust, fg = colors.sb},
+    c = {bg = colors.crust, fg = colors.textgray},
+}
+custom_catppuccin.replace = {
+    a = {bg = colors.mauve, fg = colors.crust, gui = "bold"},
+    b = {bg = colors.sb, fg = colors.text},
+    c = {bg = colors.crust, fg = colors.textgray},
+}
+custom_catppuccin.command = {
+    a = {bg = colors.sap, fg = colors.crust, gui = "bold"},
+    b = {bg = colors.sb, fg = colors.text},
+    c = {bg = colors.crust, fg = colors.textgray},
+}
+custom_catppuccin.inactive = {
+    a = {bg = colors.crust, fg = colors.textgray, gui = "bold"},
+    b = {bg = colors.crust, fg = colors.textgray},
+    c = {bg = colors.crust, fg = colors.textgray},
 }
 
 M.themes = {
     ["catppuccin"] = {
         flavor = "mocha",
-        lualine = "horizon",
+        lualine = custom_catppuccin,
     }
 }
 
